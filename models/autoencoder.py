f74f6210fc5b733041f431ce40a892e9247418b7
Rolando Innamorati <rolando.innamorati@delta2020.it>
2025-05-19 13:29:49 +0200

    Training

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -1,14 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from config import config
-
-input_length = config["autoencoder"]["window_size"]
-num_features = config["autoencoder"]["input_dim"]
-input_dim = input_length * num_features
 
 class TimeSeriesAutoencoder(nn.Module):
     def __init__(self):
@@ -16,36 +8,36 @@ class TimeSeriesAutoencoder(nn.Module):
 
         # Encoder (4 layers fully connected)
         self.encoder = nn.Sequential(
-            nn.Linear(input_dim, 256),
+            nn.Linear(204, 128),
             nn.Tanh(),
-            nn.Linear(256, 128),
-            nn.Tanh(),
+
             nn.Linear(128, 64),
             nn.Tanh(),
-            nn.Linear(64, 32),  # Bottleneck
-            nn.Tanh()
+
+            nn.Linear(64, 32),
+            nn.Tanh(),
         )
 
         # Decoder (mirror)
         self.decoder = nn.Sequential(
             nn.Linear(32, 64),
             nn.Tanh(),
+
             nn.Linear(64, 128),
             nn.Tanh(),
-            nn.Linear(128, 256),
-            nn.Tanh(),
-            nn.Linear(256, input_dim)
+
+            nn.Linear(128, 204)
         )
+
+        self._init_weights()
+
+    def _init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                nn.init.zeros_(m.bias)
 
     def forward(self, x):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
         return decoded
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = TimeSeriesAutoencoder().to(device)
-
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-criterion = nn.MSELoss()
-
-print(model)
--- models/train_autoencoder.py
+++ models/train_autoencoder.py
@@ -3,64 +3,101 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 import os
-import sys
+from autoencoder import TimeSeriesAutoencoder
+from pathlib import Path
+from tqdm import tqdm
 from torch.utils.data import DataLoader, TensorDataset
+import datetime
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from models.autoencoder import TimeSeriesAutoencoder
-from utils.preprocessing import load_and_preprocess_data
-from config import config
-
-autoencoder_config = config["autoencoder"]
-data_config = config["data"]
-training_params = {
-    "batch_size": autoencoder_config["batch_size"],
-    "learning_rate": autoencoder_config["learning_rate"],
-    "epochs": autoencoder_config["epochs"],
-    "model_save_dir": config["paths"]["model_save_dir"]
-}
+SCRIPT_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
+model_save_path = "autoencoder_model.pth"
+data_dir = SCRIPT_DIR / "../data/windows" / "raw"
+batch_size = 32
+epochs = 10
+date_column_index = 2
+lr = 0.00001
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-print("📂 Loading and preprocessing data...")
-X_train, X_val = load_and_preprocess_data()
-
-train_loader = DataLoader(TensorDataset(torch.tensor(X_train, dtype=torch.float32)), batch_size=config["batch_size"], shuffle=True)
-val_loader = DataLoader(TensorDataset(torch.tensor(X_val, dtype=torch.float32)), batch_size=config["batch_size"], shuffle=False)
+print(f"Using device: {device}")
 
 model = TimeSeriesAutoencoder().to(device)
-criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=training_params["learning_rate"])
+# criterion = nn.MSELoss()
+criterion = nn.SmoothL1Loss()
+optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=1e-5)
 
-def train_autoencoder(model, train_loader, val_loader, criterion, optimizer, epochs):
-    print("🚀 Starting training...")
+def convert_to_timestamp(date_strings):
+    timestamps = []
+    for date_str in date_strings:
+        try:
+            dt_object = datetime.datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+            timestamp = dt_object.timestamp()
+            timestamps.append(timestamp)
+        except ValueError:
+            print(f"Impossibile convertire la data: {date_str}. Usando 0.")
+            timestamps.append(0)
+    return np.array(timestamps, dtype=np.float32)
+
+def train_epoch(model, dataloader, optimizer, criterion, epoch):
+    model.train()
+    total_loss = 0
+    for batch in dataloader:
+        inputs = batch[0].to(device)
+        optimizer.zero_grad()
+
+        outputs = model(inputs)
+        loss = criterion(outputs, inputs)
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+
+    avg_loss = total_loss / len(dataloader)
+    return avg_loss
+
+def process_data_files(data_dir, model, optimizer, criterion, epochs):
+    file_paths = list(data_dir.glob("*.npz"))
     for epoch in range(epochs):
-        model.train()
-        train_loss = 0.0
-        for batch in train_loader:
-            batch = batch[0].to(device)
-            optimizer.zero_grad()
-            outputs = model(batch)
-            loss = criterion(outputs, batch)
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item()
+        print(f"Inizio Epoca {epoch + 1}")
+        epoch_loss = 0.0
+        for file_path in tqdm(file_paths, desc="Processing Files"):
+            try:
+                npzfile = np.load(file_path, allow_pickle=True)
+                window_data = npzfile['data']
+                features_data = npzfile['features']
 
-        model.eval()
-        val_loss = 0.0
-        with torch.no_grad():
-            for batch in val_loader:
-                batch = batch[0].to(device)
-                outputs = model(batch)
-                val_loss += criterion(outputs, batch).item()
+                window_data[:, date_column_index] = convert_to_timestamp(window_data[:, date_column_index])
 
-        print(
-            f"Epoch {epoch + 1}/{epochs} | Train Loss: {train_loss / len(train_loader):.6f} | Val Loss: {val_loss / len(val_loader):.6f}")
+                window_data = window_data.reshape(-1, 200)
+                features_data = features_data.reshape(-1, 4)
 
-    print("✅ Training completed!")
+                # Standardizza le 10 features di window_data separatamente
+                for i in range(10):
+                    scaler = StandardScaler()
+                    window_data[:, i * 20: (i + 1) * 20] = scaler.fit_transform(
+                        window_data[:, i * 20: (i + 1) * 20]
+                    )
 
-train_autoencoder(model, train_loader, val_loader, criterion, optimizer, config["epochs"])
+                window_data = np.concatenate((window_data, features_data), axis=1)
 
-os.makedirs(config["model_save_dir"], exist_ok=True)
-torch.save(model.state_dict(), os.path.join(config["model_save_dir"], "autoencoder.pth"))
-print("💾 Model saved successfully!")
+                np_array = np.array(window_data, dtype=np.float32)
+
+                tensor_data = torch.tensor(np_array, dtype=torch.float32).to(device)
+                dataset = TensorDataset(tensor_data)
+                dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+                epoch_loss += train_epoch(model, dataloader, optimizer, criterion, epoch)
+
+            except Exception as e:
+                print(f"Error processing {file_path}: {e}")
+        print(f"Epoch {epoch + 1} Total Loss: {epoch_loss}")
+
+process_data_files(data_dir, model, optimizer, criterion, epochs)
+
+# Save Model
+torch.save({
+    'model_state_dict': model.state_dict(),
+    'optimizer_state_dict': optimizer.state_dict(),
+}, model_save_path)
+print(f"Model saved to {model_save_path}")
